b9733b9735c1c7927f50aa12059f86ed9e6772e6
Djordje Dozet <dozet34@hotmail.com>
2016-12-10 16:56:25 +0100

    uradjene kontrole ver 1

---
+++ DeathSquad/Assets/Scripts/Ground.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ground : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if(other.gameObject.tag == "Player")
+		{
+			//Debug.Log("Aloooo");
+			other.gameObject.transform.parent.GetComponent<Player>().RestartJumps();
+		}
+	}
+}
---
+++ DeathSquad/Assets/Scripts/Player.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour 
+{
+
+	Rigidbody2D rigidbody;
+	public float horizontalMulti;
+	public float verticalMulti;
+	public float maxVelocity;
+	public float velocityAddPerFrame;
+	public float maxVeloPerX;
+	public float maxVeloPerY;
+	public float timeScale = 1;
+
+	public int jumpsLeft;
+
+
+	void Awake()
+	{
+		rigidbody = gameObject.GetComponent<Rigidbody2D>();
+		jumpsLeft = 2;
+		Time.timeScale = 1.2f;
+	}
+
+
+
+	void Update()
+	{
+		Time.timeScale = timeScale;
+		/*if(rigidbody.velocity.magnitude > maxVelocity)
+		{
+			rigidbody.velocity.Normalize();
+			rigidbody.velocity *= maxVelocity;
+			
+		}
+		*/
+		if(Input.GetKey(KeyCode.A))
+		{
+			//if(rigidbody.velocity.magnitude < maxVelocity)
+			//	rigidbody.velocity = new Vector2(rigidbody.velocity.x - velocityAddPerFrame, rigidbody.velocity.y);
+			rigidbody.AddForce(new Vector2(-1 * horizontalMulti, 0), ForceMode2D.Impulse);
+			if(-rigidbody.velocity.x > maxVeloPerX)
+			{
+				rigidbody.velocity = new Vector2(-maxVeloPerX, rigidbody.velocity.y);
+			}
+		}
+		if(Input.GetKey(KeyCode.D))
+		{
+			rigidbody.AddForce(new Vector2(1* horizontalMulti, 0), ForceMode2D.Impulse);
+			if(rigidbody.velocity.x > maxVeloPerX)
+			{
+				rigidbody.velocity = new Vector2(maxVeloPerX, rigidbody.velocity.y);
+			}
+			//if(rigidbody.velocity.magnitude < maxVelocity)
+			//rigidbody.velocity = new Vector2(rigidbody.velocity.x + velocityAddPerFrame, rigidbody.velocity.y);
+		}
+
+		if(Input.GetKeyDown(KeyCode.Space) && jumpsLeft > 0)
+		{
+			
+
+			if(jumpsLeft == 1)
+			{
+				rigidbody.AddForce(new Vector2(0, 2 * verticalMulti), ForceMode2D.Impulse);	
+			}
+			else if(jumpsLeft == 2)
+			{
+				rigidbody.AddForce(new Vector2(0, verticalMulti), ForceMode2D.Impulse);	
+			}
+
+			if(rigidbody.velocity.y > maxVeloPerY)
+			{
+				if(jumpsLeft == 1)
+				{
+					rigidbody.velocity = new Vector2(rigidbody.velocity.x, maxVeloPerY);	
+				}
+			}
+			jumpsLeft--; 
+		}
+
+
+	}
+
+	public void RestartJumps()
+	{
+		jumpsLeft = 2;
+	}
+}
