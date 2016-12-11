565470d4272858d7c476baa1776152dc7f218818
Guest User <Guest@makondas-Mac-mini.local>
2016-12-11 01:31:15 +0100

    multikara

--- DeathSquad/Assets/Scripts/Player.cs
+++ DeathSquad/Assets/Scripts/Player.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using SocketIO;
 
 public class Player : MonoBehaviour 
 {
@@ -12,51 +13,63 @@ public class Player : MonoBehaviour
 	public float maxVeloPerX;
 	public float maxVeloPerY;
 	public float timeScale = 1;
-
+	public Vector2 netMove;
 	public int jumpsLeft;
+	public string data;
 
+
+	//public GameObject player;
 
 	void Awake()
 	{
 		rigidbody = gameObject.GetComponent<Rigidbody2D>();
 		jumpsLeft = 2;
 		Time.timeScale = 1.2f;
+		//data = TestSocketIO.instance.data;
+
 	}
 
 
 
 	void Update()
 	{
-
-		if(Input.GetKey(KeyCode.A))
-		{
-			rigidbody.AddForce(new Vector2(-horizontalMulti, 0), ForceMode2D.Impulse);
-			if(-rigidbody.velocity.x > maxVeloPerX)
-			{
-				rigidbody.velocity = new Vector2(-maxVeloPerX, rigidbody.velocity.y);
+		if (data =gameObject.GetComponent<TestSocketIO> ().data != null)
+			data = gameObject.GetComponent<TestSocketIO> ().data;
+		else data = "test";
+		//stringToJson moves = JsonUtility.FromJson<stringToJson>(data);
+		if (1 == 2){
+		if (Input.GetKey (KeyCode.A)) {
+			rigidbody.AddForce (new Vector2 (-horizontalMulti, 0), ForceMode2D.Impulse);
+			if (-rigidbody.velocity.x > maxVeloPerX) {
+				rigidbody.velocity = new Vector2 (-maxVeloPerX, rigidbody.velocity.y);
 			}
 		}
-		if(Input.GetKey(KeyCode.D))
-		{
-			rigidbody.AddForce(new Vector2(1* horizontalMulti, 0), ForceMode2D.Impulse);
-			if(rigidbody.velocity.x > maxVeloPerX)
-			{
-				rigidbody.velocity = new Vector2(maxVeloPerX, rigidbody.velocity.y);
+		if (Input.GetKey (KeyCode.D)) {
+			rigidbody.AddForce (new Vector2 (1 * horizontalMulti, 0), ForceMode2D.Impulse);
+			if (rigidbody.velocity.x > maxVeloPerX) {
+				rigidbody.velocity = new Vector2 (maxVeloPerX, rigidbody.velocity.y);
 			}
 		}
 
-		if(Input.GetKeyDown(KeyCode.Space) && jumpsLeft > 0)
-		{
-			rigidbody.AddForce(new Vector2(0, verticalMulti), ForceMode2D.Impulse);	
+		if (Input.GetKeyDown (KeyCode.Space) && jumpsLeft > 0) {
+			rigidbody.AddForce (new Vector2 (0, verticalMulti), ForceMode2D.Impulse);	
 
-			if(rigidbody.velocity.y > maxVeloPerY)
-			{
-				rigidbody.velocity = new Vector2(rigidbody.velocity.x, maxVeloPerY);	
+			if (rigidbody.velocity.y > maxVeloPerY) {
+				rigidbody.velocity = new Vector2 (rigidbody.velocity.x, maxVeloPerY);	
 			}
 			jumpsLeft--; 
 		}
 
+		netMove = new Vector2 (this.transform.position.x, this.transform.position.y);
 
+		gameObject.GetComponent<NetworkMove> ().OnMove (netMove);
+
+		}
+	else {
+			
+			//this.transform.position = new Vector2 (moves.x, moves.y);	
+			Debug.Log(data);
+	}
 	}
 
 	public void RestartJumps()
@@ -68,4 +81,5 @@ public class Player : MonoBehaviour
 	{
 		//rigidbody.AddForce(new Vector2(0, verticalMulti), ForceMode2D.Impulse);
 	}
+
 }
